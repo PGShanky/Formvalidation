63004c09003e1a2a202ec2b2d0aa4a81ace9858d
PGShanky <pgshanky405@gmail.com>
2022-04-28 10:37:43 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="login.css">
+  <title>Log In To Your Account</title>
+</head>
+<body>
+<div class="form">
+  <h3>Log In</h3>
+  <ul>
+    <li>
+      <label for="username"></label><br>
+      <input placeholder="User Name" type="text" id="username"><br>
+    <li>
+      <label for="password"></label><br>
+      <input type="password" id="password" placeholder="Enter Your Password"><br>
+    </li>
+  </ul>
+
+  <button style="margin: 20px" id="button" type="submit">Log In</button>
+
+  <h3>Don't have an account?</h3><br>
+  <a href="index.php">Sign Up</a>
+
+</div>
+</body>
+</html>
